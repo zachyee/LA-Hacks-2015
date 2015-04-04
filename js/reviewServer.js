6741969e14664bccec6596ae6853365a13218e7f
zachyee <zachyee@ucla.edu>
2015-04-04 08:00:07 -0700

    Added some more reviews.

--- js/reviewServer.js
+++ js/reviewServer.js
@@ -6,12 +6,25 @@ function review(title, author, reviewer, paragraphs) {
 	this.reviewer = reviewer;
 	this.paragraphs = paragraphs;
 	this.totalReview = title + "\nWritten By " + author + 
-		"\nReviewed By " + reviewer + "\nReview: " + paragraphs;
+		"\nReviewed By " + reviewer + "\nReview: " + paragraphs + "\n";
 }
 
 var allReviews = new Array();
 allReviews.push(new review("Jane Eyre", "Charlotte Bronte", "Zachary Yee", 
 	"Crazy bitches be living in the attic."));
+allReviews.push(new review("Ulysses", "James Joyce", "Bryan Aitken",
+	"Tough to read unless you understand 40 languages and are on LSD."));
+allReviews.push(new review("The Catcher in the Rye", "J. D. Salinger", "A. Customer",
+	"The man who wrote this should be in an asylum. Total waste of paper."));
+allReviews.push(new review("The Great Gatsby", "F. Scott Fitzgerald", "Stephen Roberts",
+	"Excellent substitute for valium... The most artificially inflated phenomenon since "
+	+ "\nthe 1929 stock market, and likewise it seems like a worthy instigator of "
+	+ "\nthe Great Depression."));
+allReviews.push(new review("1984", "George Orwell", "Shar Tampeka",
+	"High expectations only to suffer through a drab chain of non sequitur events,"
+	+ " thoroughly lacking any explanations at all"));
+allReviews.push(new review("Crime and Punishment", "Ryodor Dostoyevsky", "Zachary Yee",
+	"Have you prepared for a 10-day marathon?"));
 
 // http://localhost:8888/
 
